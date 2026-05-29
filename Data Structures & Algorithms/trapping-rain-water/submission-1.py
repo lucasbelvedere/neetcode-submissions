209f806f4ab82d7802f6ce80e39aab6553445e36
Lucas Belvedere <98793453+lucasbelvedere@users.noreply.github.com>
2026-05-29 08:10:43 -0700

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # if a matrix was given to us rather than a list, then a prefix sum approach
+        # could be used, and subset of the array could give us the answer 
+        # (Subset B (INDEX) - Subset A (INDEX) - 1 - NumBarsBetweenBandA)
+
+        lenHeight = len(height)
+        if lenHeight <= 2:
+            return 0
+
+        maxLeft = [0] * lenHeight
+        maxRight = [0] * lenHeight
+        minHeight = [0] * lenHeight
+
+        # populate maxLeft with preSum
+        for i in range(lenHeight):
+            maxLeft[i] = max(maxLeft[i - 1], height[i - 1]) if i != 0 else 0
+
+        # populate minLeft with postSum
+        for i in range(lenHeight - 1, -1, -1):
+            maxRight[i] = max(maxRight[i + 1], height[i + 1]) if i < lenHeight - 1 else 0
+
+        # populate minHeight
+        for i in range(lenHeight):
+            minHeight[i] = min(maxLeft[i], maxRight[i])
+
+        areaOfWater = 0
+        for i in range(1, lenHeight - 1): # jump left and right corner, since there is no way to have bars both sides
+            areaOfWater += (minHeight[i] - height[i]) if height[i] < minHeight[i] else 0
+        return areaOfWater
+
+        
+
+        
